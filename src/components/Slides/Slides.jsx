6495360644a4a12de8f2a4bf98082db0d8a57957
Quillon <quillonthegeek@gmail.com>
2023-03-31 07:57:53 -0700

    footer & Project Cards

--- src/components/Slides/Slides.jsx
+++ src/components/Slides/Slides.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './Slides.scss';
 import Slider from 'infinite-react-carousel';
-import CatCard from '../catCard/CatCard';
-import { cards } from './../../data';
 
 
-const Slides = () => {
+
+const Slides = ({children, arrowsScroll, slidesToShow}) => {
   return (
   <div className="slides">
       <div className="container">
-         <Slider slidesToShow={5} arrowsScroll={5}>
-          {cards.map(card => (<CatCard item={card} key ={card.id}/>))}
+         <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
+          {children}
   </Slider>
     </div>
       </div>
