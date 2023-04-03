95ac366e0e0d6b9e08bea05988dd3267e00be5dd
Quillon <quillonthegeek@gmail.com>
2023-04-03 09:00:37 -0700

    Gigs and GigsCard

--- src/components/projectCard/ProjectCard.jsx
+++ src/components/projectCard/ProjectCard.jsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import './ProjectCard.scss';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./ProjectCard.scss";
+import { Link } from "react-router-dom";
 
-const ProjectCard =  ({item}) => {
+const ProjectCard = ({ item }) => {
   return (
-    <Link to='/' className='link'>
-        <div className='projectCard'>
-      <img src={item.img} alt="" />
+    <Link to="/" className="link">
+      <div className="projectCard">
+        <img src={item.img} alt="" />
 
-      <div className="info">
-      <img src={item.pp} alt="" />
-      
-      <div className="text">
-      <h2>{item.cat}</h2>
-      <span>{item.username}</span>
-      </div>
+        <div className="info">
+          <img src={item.pp} alt="" />
 
-      </div>
+          <div className="text">
+            <h2>{item.cat}</h2>
+            <span>{item.username}</span>
+          </div>
         </div>
+      </div>
     </Link>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
