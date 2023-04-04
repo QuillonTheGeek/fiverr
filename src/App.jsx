a751ba0f480f622234cf6079e51bc8d9b1b7116c
Quillon <quillonthegeek@gmail.com>
2023-04-04 05:48:11 -0700

    Gigs

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,76 @@
-import React from 'react'
+import React from "react";
 
-import {
-  createBrowserRouter,
-  Outlet,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
-import Navbar from './components/Navbar/Navbar';
-import Home from './pages/home/Home';
-import Gigs from './pages/gigs/Gigs';
-import Gig from './pages/gig/Gig';
-import MyGigs from './pages/myGigs/MyGigs';
-import Add from './pages/add/Add';
-import Messages from './pages/messages/Messages';
-import Message from './pages/message/Message';
-import Orders from './pages/order/Orders';
-import Footer from './components/Footer/Footer';
-import './App.scss'
- 
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/home/Home";
+import Gigs from "./pages/gigs/Gigs";
+import Gig from "./pages/gig/Gig";
+import MyGigs from "./pages/myGigs/MyGigs";
+import Add from "./pages/add/Add";
+import Messages from "./pages/messages/Messages";
+import Message from "./pages/message/Message";
+import Orders from "./pages/order/Orders";
+import Footer from "./components/Footer/Footer";
+import "./App.scss";
+
 const Layout = () => {
-    return(
-        <>
-        <Navbar/>
-        <Outlet/>
-        <Footer/>
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout/>,
-    children:[
-        {
-            path:'/',
-            element:<Home/>
-        },
-        {
-            path:'/gig/:id',
-            element:<Gig/>
-        },
-        {
-            path:'/gigs',
-            element:<Gigs/>
-        },
-        {
-            path:'/orders',
-            element:<Orders/>
-        },
-        {
-            path:'/mygigs',
-            element:<MyGigs/>
-        },
-        {
-            path:'/add',
-            element:<Add/>
-        },
-        {
-            path:'/messages',
-            element:<Messages/>
-        },
-        {
-            path:'/message/:id',
-            element:<Message/>
-        },
-    ]
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/gig/:id",
+        element: <Gig />,
+      },
+      {
+        path: "/gigs",
+        element: <Gigs />,
+      },
+      {
+        path: "/orders",
+        element: <Orders />,
+      },
+      {
+        path: "/mygigs",
+        element: <MyGigs />,
+      },
+      {
+        path: "/add",
+        element: <Add />,
+      },
+      {
+        path: "/messages",
+        element: <Messages />,
+      },
+      {
+        path: "/message/:id",
+        element: <Message />,
+      },
+    ],
   },
 ]);
 
-
 function App() {
   return (
-    <div className='app'>
-        <RouterProvider router={router} />
+    <div className="app">
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,58 +1,65 @@
-import './Home.scss';
-import React from 'react';
-import Featured from '../../components/Featured/Featured';
-import TrustedBy from '../../components/trustedBy/TrustedBy';
-import Slides from '../../components/Slides/Slides';
-import { cards } from '../../data';
-import { projects } from '../../data';
-import CatCard from '../../components/catCard/CatCard';
-import ProjectCard from '../../components/projectCard/ProjectCard';
+import "./Home.scss";
+import React from "react";
+import Featured from "../../components/Featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slides from "../../components/Slides/Slides";
+import { cards } from "../../data";
+import { projects } from "../../data";
+import CatCard from "../../components/catCard/CatCard";
+import ProjectCard from "../../components/projectCard/ProjectCard";
 
 const Home = () => {
   return (
-    <div className='home'>
-      <Featured/>
-      <TrustedBy/>
+    <div className="home">
+      <Featured />
+      <TrustedBy />
 
-      <Slides  slidesToShow={5} arrowsScroll={5}>
-        {cards.map(card => (<CatCard item={card} key ={card.id}/>))}
+      <Slides slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard item={card} key={card.id} />
+        ))}
       </Slides>
 
       <div className="features">
         <div className="container">
           <div className="item">
             <h1>A whole world of freelance talent at your fingertips</h1>
-       <div className="title">
-        <img src="./img/check.png" alt="" />
-        The best of every budget
-       </div>
-       <p>
-        Find high-quality services at every price point. No hourly rates, just project-based pricing.
-       </p>
-       <div className="title">
-        <img src="./img/check.png" alt="" />
-        Quality work done quickly
-       </div>
-       <p>
-        Find the right freelancer to begin working on your project within minutes.
-       </p>
-       <div className="title">
-        <img src="./img/check.png" alt="" />
-        Protected payments, every time
-       </div>
-       <p>
-       {`Always know what you'll pay upfront. Your payment isn't released until you approve the work`}
-       </p>
-       <div className="title">
-        <img src="./img/check.png" alt="" />
-        24/7 support
-       </div>
-       <p>
-       Questions? Our round-the-clock support team is available to help anytime, anywhere.
-       </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best of every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Quality work done quickly
+            </div>
+            <p>
+              Find the right freelancer to begin working on your project within
+              minutes.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Protected payments, every time
+            </div>
+            <p>
+              {`Always know what you'll pay upfront. Your payment isn't released until you approve the work`}
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              24/7 support
+            </div>
+            <p>
+              Questions? Our round-the-clock support team is available to help
+              anytime, anywhere.
+            </p>
           </div>
           <div className="item">
-            <video src="./img/video.mp4" controls preload='auto'> </video>
+            <video src="./img/video.mp4" controls preload="auto">
+              {" "}
+            </video>
           </div>
         </div>
       </div>
@@ -60,43 +67,49 @@ const Home = () => {
       <div className="features dark">
         <div className="container">
           <div className="item">
-           <div className="letter">
-             <h1>fiverr </h1>
+            <div className="letter">
+              <h1>fiverr </h1>
               <h3> business.</h3>
-           </div>
-            <h2>A business solution 
-              <br/> designed for <i style={{fontWeight:300}}> teams</i>
-              </h2>
-              <p>
-                Upgrade to a curated experience packed with tools and benefits, dedicated to businesses
-              </p>
-          <div className="title">
-            <img src="./img/check.png" alt="" />
-            Connect to freelancers with proven business experience
+            </div>
+            <h2>
+              A business solution
+              <br /> designed for <i style={{ fontWeight: 300 }}> teams</i>
+            </h2>
+            <p>
+              Upgrade to a curated experience packed with tools and benefits,
+              dedicated to businesses
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Connect to freelancers with proven business experience
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Get matched with the perfect talent by a customer success manager
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Manage teamwork and boost productivity with one powerful workspace
+            </div>
+            <button>Explore Fiverr Business</button>
           </div>
-          <div className="title">
-            <img src="./img/check.png" alt="" />
-            Get matched with the perfect talent by a customer success manager
-          </div>
-          <div className="title">
-            <img src="./img/check.png" alt="" />
-            Manage teamwork and boost productivity with one powerful workspace
-          </div>
-          <button>Explore Fiverr Business</button>
-           </div>
 
           <div className="item">
-           <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_1.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624757/business-desktop-870-x1.png" alt="" />
+            <img
+              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_1.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624757/business-desktop-870-x1.png"
+              alt=""
+            />
           </div>
         </div>
       </div>
 
-       <Slides  slidesToShow={4} arrowsScroll={4}>
-        {projects.map(card => (
-        <ProjectCard item={card} key ={card.id}/>))}
+      <Slides slidesToShow={4} arrowsScroll={4}>
+        {projects.map((card) => (
+          <ProjectCard item={card} key={card.id} />
+        ))}
       </Slides>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
