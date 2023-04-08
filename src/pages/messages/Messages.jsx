9aa57c30dff3122e4e0cfca34a7cc29cb7b0c5af
Quillon <quillonthegeek@gmail.com>
2023-04-08 10:51:51 -0700

    Orders & Messages

--- src/pages/messages/Messages.jsx
+++ src/pages/messages/Messages.jsx
@@ -1,10 +1,95 @@
-import './Messages.scss'
-import React from 'react';
+import "./Messages.scss";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Messages = () => {
-  return (
-    <div>Messages</div>
-  )
-}
+  const currentUser = {
+    id: 1,
+    username: "Quillon",
+    isSeller: true,
+  };
 
-export default Messages
+  const message =
+    "Lorem ipsum dolor, sit amet consectetur placeat praesentium a pariatur reprehe aliquam sapiente?";
+
+  return (
+    <div className="messages">
+      <div className="container">
+        <div className="title">
+          <h1>Messages</h1>
+        </div>
+
+        <table>
+          <tr>
+            <th>Buyer</th>
+            <th>Last Message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td>Quillon</td>
+            <td className="active">
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>30 mins ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>Quillon</td>
+            <td className="active">
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 hr ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>Quillon</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>3 hrs ago</td>
+          </tr>
+          <tr>
+            <td>Quillon</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>18 hrs ago</td>
+          </tr>
+          <tr>
+            <td>Quillon</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 Day ago</td>
+          </tr>
+          <tr>
+            <td>Quillon</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>2 Day ago</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Messages;
--- src/pages/order/Orders.jsx
+++ src/pages/order/Orders.jsx
@@ -1,10 +1,134 @@
-import React from 'react';
-import './Orders.scss'
+import "./Orders.scss";
+import React from "react";
 
 const Orders = () => {
-  return (
-    <div>Orders</div>
-  )
-}
+  const currentUser = {
+    id: 1,
+    username: "Quillon",
+    isSeller: true,
+  };
 
-export default Orders
+  return (
+    <div className="orders">
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+        </div>
+
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th> {currentUser?.isSeller ? "Buyer" : "Seller"} </th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Stunning concept art</td>
+            <td>
+              59.<sup>99</sup>
+            </td>
+            <td>Quillon</td>
+            <td>
+              <img className="message" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/5792641/pexels-photo-5792641.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+            </td>
+            <td>Ai generated concept art</td>
+            <td>
+              120.<sup>99</sup>
+            </td>
+            <td>Quillon</td>
+            <td>
+              <img className="message" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/16033965/pexels-photo-16033965.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+            </td>
+            <td>High quality digital character</td>
+            <td>
+              79.<sup>99</sup>
+            </td>
+            <td>Quillon</td>
+            <td>
+              <img className="message" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
+                alt=""
+              />
+            </td>
+            <td>Illustration hyper realistic painting</td>
+            <td>
+              119.<sup>99</sup>
+            </td>
+            <td>Quillon</td>
+            <td>
+              <img className="message" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1e9-1f1ea.png"
+                alt=""
+              />
+            </td>
+            <td>Original ai generated digital art</td>
+            <td>
+              59.<sup>99</sup>
+            </td>
+            <td>Quillon</td>
+            <td>
+              <img className="message" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/14019734/pexels-photo-14019734.jpeg?auto=compress&cs=tinysrgb&w=400"
+                alt=""
+              />
+            </td>
+            <td>Text based ai generated art</td>
+            <td>
+              110.<sup>99</sup>
+            </td>
+            <td>Quillon</td>
+            <td>
+              <img className="message" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
